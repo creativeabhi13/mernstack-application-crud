8aab18ee6cc56cce4f669751833141bc10b26ec0
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2024-07-10 17:16:04 +0530

    updated with delete and update
    update user by id
    delete user by id

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,31 +1,5 @@
 const User = require("../models/users.js");
 
-
-// get user by id from the database
-const getUser = async (req,res) =>{
-  try {
-    const {id} = req.params;
-    const user = await User.findById(id);
-    res.status(200).json({ user });
-  }catch (error){
-    res.status(500).json({message: error.message});
-  }
-}
-
-
-// get all users from the database
-const getUsers = async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.status(200).json({ users });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-
-
-
 // create a new user
 const createUser = async (req, res) => {
   try {
@@ -39,8 +13,62 @@ const createUser = async (req, res) => {
   }
 };
 
+// get user by id from the database
+const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// get all users from the database
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json({ users });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// update user by id in the database
+const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByIdAndUpdate(id, req.body);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const updateUser = await User.findById(id);
+    res.status(200).json(updateUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// delete user by id from the database
+const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByIdAndDelete(id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// export the functions
+
 module.exports = {
- createUser,
-getUsers,
-getUser
+  createUser,
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser
 };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,7 @@
 const express = require("express"); 
 const User = require("../models/users.js");
 const router = express.Router();
-const { createUser,getUsers,getUser} = require("../controllers/userController.js");
+const { createUser,getUsers,getUser,updateUser,deleteUser} = require("../controllers/userController.js");
 
 // Add new user to the database
 router.post("/createuser",createUser); 
@@ -12,7 +12,11 @@ router.get("/",getUsers);
 // Get user by id from the database
 router.get("/:id",getUser);
 
+// Update user by id in the database
+router.put("/:id",updateUser);
 
+// Delete user by id from the database
+router.delete("/:id",deleteUser);
 
 
 // Export the router
